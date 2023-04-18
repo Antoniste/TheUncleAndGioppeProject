a6e8a11184eb493d27aea8a7d2c45e6feb24aa8f
Antonio Di Paolo <antonistejob@gmail.com>
2023-04-18 09:53:25 +0200

    Base

--- app/src/main/java/com/example/theuncleandgioppeproject/MainActivity.kt
+++ app/src/main/java/com/example/theuncleandgioppeproject/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.theuncleandgioppeproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.theuncleandgioppeproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/com/example/theuncleandgioppeproject/UncleRepository.kt
@@ -0,0 +1,4 @@
+package com.example.theuncleandgioppeproject
+
+class UncleRepository {
+}
