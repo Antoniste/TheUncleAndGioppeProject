34577ce6ffc826a856e621fd3969f45eb93573c1
GiuseppeTriunfo <giuseppe.triunfo@aesys.tech>
2023-05-02 11:15:36 +0200

    add logout in homePage

--- app/src/main/java/com/example/theuncleandgioppeproject/core/network/base/network/Apiservice.kt
+++ app/src/main/java/com/example/theuncleandgioppeproject/core/network/base/network/Apiservice.kt
@@ -6,8 +6,8 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
-    @GET("/v1/public/series?ts=1682584667000&apikey=781fb826b12d89b527431ec48e0fce42&hash=0cbafdcbe4eb6e309f365f74f7c34bdc")
-    suspend fun getResponse(): Response<MarvelCharacterResponse>
+    @GET("/v1/public/series")
+    suspend fun getResponse(@Query("ts") ts: String,@Query("apikey") key:String,@Query("hash") hash:String): Response<MarvelCharacterResponse>
 
 
 }
