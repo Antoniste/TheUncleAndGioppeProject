e1a8a21027bca4048ff82ad5adc78a313df7121a
GiuseppeTriunfo <giuseppe.triunfo@aesys.tech>
2023-05-04 14:11:05 +0200

    add sharePref with userName Home

--- app/src/main/java/com/example/theuncleandgioppeproject/HomeFragment.kt
+++ app/src/main/java/com/example/theuncleandgioppeproject/HomeFragment.kt
@@ -1,16 +1,16 @@
 package com.example.theuncleandgioppeproject
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.theuncleandgioppeproject.adapter.RecyclerViewAdapterHome
+import com.example.theuncleandgioppeproject.adapter.RecyclerViewAdapterImage
 import com.example.theuncleandgioppeproject.databinding.FragmentHomeBinding
+import com.example.theuncleandgioppeproject.model.ImageModel
 import com.example.theuncleandgioppeproject.viewModel.HardViewModel
 import com.example.theuncleandgioppeproject.viewModel.LoginViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,6 +19,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
     private lateinit var adapterRe: RecyclerViewAdapterHome
+    private lateinit var adapterImage: RecyclerViewAdapterImage
     private val loginViewModel: LoginViewModel by activityViewModels()
     private val hardViewModel: HardViewModel by activityViewModels()
 
@@ -30,17 +31,29 @@ class HomeFragment : Fragment() {
         return binding.root
     }
 
-    @SuppressLint("SuspiciousIndentation")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         hardViewModel.getUser()
         adapterRe = RecyclerViewAdapterHome()
-
+        val imageList =ArrayList<ImageModel>()
+        imageList.add(ImageModel(R.drawable.ironman_paper_face_masks_01))
+        imageList.add(ImageModel(R.drawable.spider_man_face_mask_coloring_book_clip_art_spider_man_mask_cliparts))
+        imageList.add(ImageModel(R.drawable._58_1584724_thanos_comic_marvel_freetoedit_thanos_comic_png_transparent))
        /* binding.editSearch.doOnTextChanged { text, _, _, _ ->
             hardViewModel.
         }*/
+        adapterImage= RecyclerViewAdapterImage()
+        adapterImage.apply {
+            submitList(imageList)
+        }
+        binding.carouselRecyclerview.adapter = adapterImage
+        binding.carouselRecyclerview.apply {
+            set3DItem(true)
+            setAlpha(true)
+            setInfinite(false)
+        }
 
-        binding.ironManMaterialButton.setOnClickListener{
+       /* binding.ironManMaterialButton.setOnClickListener{
             hardViewModel.getIronManData("iron man")
             hardViewModel.event.observe(viewLifecycleOwner) {
                 adapterRe.apply {
@@ -55,7 +68,7 @@ class HomeFragment : Fragment() {
                     adapter = adapterRe
                 }
             }
-        }
+        }*/
         hardViewModel.userName.observe(viewLifecycleOwner){
             binding.nameHome.text=it
         }
