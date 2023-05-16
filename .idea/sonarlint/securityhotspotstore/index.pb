a4bb5110a3f89ef2a1504ea788e33565f237ec27
Antonio Di Paolo <antonistejob@gmail.com>
2023-05-16 15:17:02 +0200

    add part of NFC

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -3,11 +3,7 @@ o
 ?app/src/main/java/com/example/theuncleandgioppeproject/MyApp.kt,b/3/b346c5a57abab07d26e907c413440a8b929ab705
 <
 build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
-x
-Happ/src/main/java/com/example/theuncleandgioppeproject/SignUpFragment.kt,9/7/971d69119230020e674a2fad279c1761395a4b0a
 @
 app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
-{
-Kapp/src/main/java/com/example/theuncleandgioppeproject/view/MainActivity.kt,8/7/871362e5962fac6eada60906cc4b79ba026f804f
 y
 Iapp/src/main/java/com/example/theuncleandgioppeproject/WelcomeFragment.kt,d/8/d8cba38c9f89cab14ab5afe8b073120d8e4d0a00
--- app/src/main/java/com/example/theuncleandgioppeproject/view/MainActivity.kt
+++ app/src/main/java/com/example/theuncleandgioppeproject/view/MainActivity.kt
@@ -1,29 +1,51 @@
 package com.example.theuncleandgioppeproject.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.NfcA
 import android.os.Bundle
-import androidx.activity.OnBackPressedCallback
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.theuncleandgioppeproject.R
 import com.example.theuncleandgioppeproject.databinding.ActivityMainBinding
-
+import com.example.theuncleandgioppeproject.utils.Constants.NFC_READ_COMMAND
+import android.nfc.*
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
+    private val nfcAdapter by lazy { NfcAdapter.getDefaultAdapter(this) }
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        var tagFromIntent: Tag? = intent?.getParcelableExtra(NfcAdapter.EXTRA_TAG)
+        val nfc = NfcA.get(tagFromIntent)
+        val atqa: ByteArray = nfc.atqa
+        val sak: Short = nfc.sak
+        nfc.connect()
+        val isConnected = nfc.isConnected
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment
-            navController = navHostFragment.navController
-
+        if (isConnected) {
+            val receivedData: ByteArray = nfc.transceive(NFC_READ_COMMAND)
+        }else{
+        Log.e("ans", "Not connected")
     }
+}
+
+
+override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    val binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    val navHostFragment =
+        supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment
+    navController = navHostFragment.navController
+
+}
 }
