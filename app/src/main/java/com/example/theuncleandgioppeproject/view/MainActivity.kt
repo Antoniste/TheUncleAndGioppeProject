852c720fb0541a6f1e05cf97765548bde0503475
Antonio Di Paolo <antonistejob@gmail.com>
2023-05-18 10:17:36 +0200

    add NFC Reader

--- app/src/main/java/com/example/theuncleandgioppeproject/view/MainActivity.kt
+++ app/src/main/java/com/example/theuncleandgioppeproject/view/MainActivity.kt
@@ -1,83 +1,100 @@
 package com.example.theuncleandgioppeproject.view
 
 import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_MUTABLE
 import android.content.Intent
 import android.content.IntentFilter
+import android.nfc.NdefMessage
 import android.nfc.NfcAdapter
-import android.nfc.Tag
-import android.nfc.tech.NfcA
+import android.nfc.tech.NfcF
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.theuncleandgioppeproject.R
 import com.example.theuncleandgioppeproject.databinding.ActivityMainBinding
-import android.nfc.*
-import com.example.theuncleandgioppeproject.core.network.base.network.Constants.NFC_READ_COMMAND
-import com.example.theuncleandgioppeproject.core.network.base.network.Constants.e
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
-    private val nfcAdapter by lazy { NfcAdapter.getDefaultAdapter(this) }
-    override fun onNewIntent(intent: Intent?) {
-        super.onNewIntent(intent)
-        var tagFromIntent: Tag? = intent?.getParcelableExtra(NfcAdapter.EXTRA_TAG)
-        val nfc = NfcA.get(tagFromIntent)
-        val atqa: ByteArray = nfc.atqa
-        val sak: Short = nfc.sak
-        nfc.connect()
-        val isConnected = nfc.isConnected
 
-        if (isConnected) {
-            val receivedData: ByteArray = nfc.transceive(NFC_READ_COMMAND)
-
-        }else{
-        Log.e("ans", "Not connected")
+    private var intentFiltersArray: Array<IntentFilter>? = null
+    private val techListsArray = arrayOf(arrayOf(NfcF::class.java.name))
+    private val nfcAdapter: NfcAdapter? by lazy {
+        NfcAdapter.getDefaultAdapter(this)
     }
-}
+    private var pendingIntent: PendingIntent? = null
 
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
 
-override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
+        val action = intent.action
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED == action) {
+            val parcelables = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
+            with(parcelables) {
+                try {
+                    val inNdefMessage = this?.get(0) as NdefMessage
+                    val inNdefRecords = inNdefMessage.records
+                    //if there are many records, you can call inNdefRecords[1] as array
+                    val inMessage = String(inNdefRecords[0].payload)
+                    val machineid = inMessage.drop(3);
 
-    val binding = ActivityMainBinding.inflate(layoutInflater)
-    setContentView(binding.root)
-
-    val navHostFragment =
-        supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment
-    navController = navHostFragment.navController
-
-}
-    private fun enableForegroundDispatch(activity: AppCompatActivity, adapter: NfcAdapter?) {
-        val intent = Intent(activity.applicationContext, activity.javaClass)
-        intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
-        val pendingIntent = PendingIntent.getActivity(
-            activity.applicationContext,
-            0,
-            intent,
-            PendingIntent.FLAG_IMMUTABLE
-        )
-        val filters = arrayOfNulls<IntentFilter>(1)
-        val techList = arrayOf<Array<String>>()
-        filters[0] = IntentFilter()
-        with(filters[0]) {
-            this?.addAction(NfcAdapter.ACTION_NDEF_DISCOVERED)
-            this?.addCategory(Intent.CATEGORY_DEFAULT)
-            try {
-                this?.addDataType("text/plain")
-            } catch (ex: IntentFilter.MalformedMimeTypeException) {
-                throw RuntimeException(e)
+                    Toast.makeText(
+                        applicationContext,
+                        "The Bobbers is: $machineid",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } catch (ex: Exception) {
+                    Toast.makeText(
+                        applicationContext,
+                        "There is no information on the device please write and retry ",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
             }
         }
-        adapter?.enableForegroundDispatch(activity, pendingIntent, filters, techList)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //nfc process start
+        pendingIntent = PendingIntent.getActivity(
+            this, 0, Intent(this, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), FLAG_MUTABLE
+        )
+        val ndef = IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED)
+
+        try {
+            ndef.addDataType("text/plain")
+        } catch (e: IntentFilter.MalformedMimeTypeException) {
+            throw RuntimeException("fail", e)
+        }
+
+        intentFiltersArray = arrayOf(ndef)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
     override fun onResume() {
         super.onResume()
-        enableForegroundDispatch(this, this.nfcAdapter)
+        nfcAdapter?.enableForegroundDispatch(
+            this,
+            pendingIntent,
+            intentFiltersArray,
+            techListsArray
+        )
     }
 
+    override fun onPause() {
+        if (this.isFinishing) {
+            nfcAdapter?.disableForegroundDispatch(this)
+        }
+        super.onPause()
+    }
 }
